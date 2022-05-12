b80eef70e3827b51c726345f3e2e2ee66675ff8b
khalil224 <khalilislamju@gmail.com>
2022-05-12 12:50:15 +0600

    information added

---
+++ src/Pages/Home/Information/Information.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import InfoCart from './InfoCart';
+import clock from '../../../assets/icons/clock.svg'
+import marker from '../../../assets/icons/marker.svg'
+import phone from '../../../assets/icons/phone.svg'
+
+
+const Information = () => {
+    return (
+        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5'>
+            <InfoCart img={clock}></InfoCart>
+            <InfoCart img={marker}></InfoCart>
+            <InfoCart img={phone}></InfoCart>
+        </div>
+    );
+};
+
+export default Information;
